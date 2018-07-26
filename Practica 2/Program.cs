d04e03c5e78dc4628686116eb8929e1100570289
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-26 17:43:44 -0400

    Actualizar

---
+++ Practica 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n;
+			Console.WriteLine("Ingrese numero:");
+			
+			n = int.Parse(Console.ReadLine());
+			
+
+			if ((n %2) == 0)
+
+				Console.WriteLine("el numero es par");
+
+			else
+				Console.WriteLine("el numero es impar");
+
+			Console.ReadKey(); 
+		}
+	}
+}
+
